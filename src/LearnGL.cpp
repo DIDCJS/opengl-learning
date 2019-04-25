1330a0e71505fbe2dcb27e5aabe2466971fc3f82
DIDCJS <1029518371@qq.com>
2019-04-25 17:37:27 +0800

    入门--》》变换

--- src/LearnGL.cpp
+++ src/LearnGL.cpp
@@ -8,22 +8,22 @@
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//#include <GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 
 void LearnGL::Init(unsigned char* pT0, int nT0W, int nT0H, int nT0C,
-								unsigned char* pT1, int nT1W, int nT1H, int nT1C/*,
-									unsigned char* pT2, int nT2W, int nt2H, int nT2C*/) {
+	unsigned char* pT1, int nT1W, int nT1H, int nT1C/*,
+		unsigned char* pT2, int nT2W, int nt2H, int nT2C*/) {
 	glProgram = new GLuint[NUM_SHADERS];
 	_render = new GLRenders[NUM_SHADERS];
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
-	
-	
+
+
 	CreateTexture(m_TexImages[TEXTURE0], nT0W, nT0H, GL_RGBA, GL_UNSIGNED_BYTE, GL_RGBA, GL_LINEAR, GL_LINEAR, pT0);
 	//CreateTexture(m_TexImages[TEXTURE1], nT1W, nT1W, GL_RGBA, GL_UNSIGNED_BYTE, GL_RGBA, GL_LINEAR, GL_LINEAR, pT1);
 	//CreateTexture(m_TexImages[TEXTURE2], nT2W, nT2W, GL_RGBA, GL_UNSIGNED_BYTE, GL_RGBA, GL_LINEAR, GL_LINEAR, pT2);
-}	
+}
 
 
 
@@ -49,35 +49,40 @@ void LearnGL::LearnGL_Main() {
 		//ÎÆÀí×ø±ê
 		_render[SHADER_LEARN].setVectexAttribute("aTexCoord", 2, 5 * sizeof(float), (const float*)(3 * sizeof(float)));
 	}
-	
+
 	glUseProgram(glProgram[SHADER_LEARN]);
 
 	//uniform,attribute
-	{
+
 #if 0 
-		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
-		glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-		glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-		glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+	glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
-		glm::mat4 view;
-		view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
-			glm::vec3(0.0f, 0.0f, 0.0f),
-			glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::mat4 view;
+	view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(0.0f, 1.0f, 0.0f));
 
 
-		glm::mat4 trans;
-		trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-		trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-		_render[SHADER_LEARN].setMat4Uniform("transform", trans);
+	glm::mat4 trans;
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	_render[SHADER_LEARN].setMat4Uniform("transform", trans);
 #endif
 
 
-		_render[SHADER_LEARN].setTextureID("ourTexture0", GL_TEXTURE0, 0, m_TexImages[TEXTURE0].glTexture, 0);
-		//_render[SHADER_LEARN].setTextureID("ourTexture2", GL_TEXTURE1, 1, m_TexImages[TEXTURE1].glTexture, 0);
-	}
+	_render[SHADER_LEARN].setTextureID("ourTexture0", GL_TEXTURE0, 0, m_TexImages[TEXTURE0].glTexture, 0);
+	//_render[SHADER_LEARN].setTextureID("ourTexture2", GL_TEXTURE1, 1, m_TexImages[TEXTURE1].glTexture, 0);
+
+
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+	trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+	_render[SHADER_LEARN].setMat4Uniform("transform", trans);
 
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -85,7 +90,7 @@ void LearnGL::LearnGL_Main() {
 
 void LearnGL::Release() {
 	delete glProgram;
-	if(VAO!=0) glDeleteVertexArrays(1, &VAO);
+	if (VAO != 0) glDeleteVertexArrays(1, &VAO);
 	if (VBO != 0) glDeleteBuffers(1, &VBO);
 	if (EBO != 0) glDeleteBuffers(1, &EBO);
 
