5ad9a2416eb31fb33349dea57d8c178106fa187f
DIDCJS <1029518371@qq.com>
2019-12-15 14:54:17 +0800

    [feature]cmake同步shaders资源

--- shaders/mesh.fs
+++ shaders/mesh.fs
@@ -1,12 +1,66 @@
 #version 330 core  
-// in vec3 Normal;
-// in vec3 FragPos;
-// in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
+struct PointLight {
+    vec3 position;
+
+    float constant;
+    float linear;
+    float quadratic;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};  
+
+
 // uniform vec3 lightPos;
 // uniform vec3 objectColor;
-// uniform vec3 viewPos;
+uniform vec3 viewPos;
+
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_specular1;
+
+#define NR_POINT_LIGHTS 1
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    // 漫反射着色
+    float diff = max(dot(normal, lightDir), 0.0);
+    // 镜面光着色
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    // 衰减
+    float distance    = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + 
+                 light.quadratic * (distance * distance));    
+    // 合并结果
+    vec3 ambient  = light.ambient  * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 diffuse  = light.diffuse  * diff * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 specular = light.specular * spec * vec3(texture(texture_specular1, TexCoords));
+    // ambient  *= attenuation;
+    // diffuse  *= attenuation;
+    // specular *= attenuation;
+    return (ambient + diffuse + specular);
+}
+
 
 void main(){
-	gl_FragColor = vec4(1.0);
+	// gl_FragColor = texture(texture_specular1, TexCoords);
+	// gl_FragColor = texture(texture_diffuse1, TexCoords);
+	//gl_FragColor = texture(texture_specular1, TexCoords);
+	 //gl_FragColor = vec4(1.0);
 
+	vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 result = vec3(0.0);
+    // 第二阶段：点光源
+    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);    
+
+    gl_FragColor = vec4(result, 1.0);
 }
